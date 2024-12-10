fd0834bd321d975847d4f5a2ddf327a2bc2da18b
Ste <stefanobgee@live.it>
2024-12-10 13:05:31 +0100

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,9 +4,13 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const handleAddToCart = (plant) => {
-        dispatch(addItem(plant)); 
-      };
+    const handleAddToCart = (product) => {
+      dispatch(addItem(product));
+      setAddedToCart((prevState) => ({
+      ...prevState,
+     [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+   }));
+};
     const plantsArray = [
         {
             category: "Air Purifying Plants",
